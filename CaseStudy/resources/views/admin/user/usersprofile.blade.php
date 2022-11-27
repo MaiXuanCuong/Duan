753e128cfba27d96f0dd3a1bc88df6953f4faca2
MaiXuanCuong <105618526+MaiXuanCuong@users.noreply.github.com>
2022-11-27 09:55:10 +0700

    Update usersprofile.blade.php

--- CaseStudy/resources/views/admin/user/usersprofile.blade.php
+++ CaseStudy/resources/views/admin/user/usersprofile.blade.php
@@ -205,22 +205,7 @@
                                         </div> 
                                     </div>
                             </div>
-                           
-                          
-{{-- 
-                          <div class="row mb-3">
-                                <label for="Email" class="col-md-4 col-lg-3 col-form-label">Email</label>
-                                <div class="col-md-8 col-lg-9">
-                                    <input name="email" type="email" class="form-control" id="Email"
-                                        value="{{ Auth()->user()->email }}">
-                                </div>
-                            </div> --}}
-
-                           
                             </form><!-- End Profile Edit Form -->
-
-                        </div>
-
 
                         <div class="tab-pane pt-3" id="profile-change-password">
                             <!-- Change Password Form -->
